68f72752cf5d59ea6e17c703c4f68ac82953908a
floffah <therealfloffah@gmail.com>
2021-03-28 13:51:31 +0100

    run command & exclude by id

--- src/build.ts
+++ src/build.ts
@@ -2,9 +2,9 @@ import "source-map-support/register";
 import {program} from "commander";
 import {existsSync, readFileSync} from "fs";
 import {resolve} from "path";
-import {buildCommands} from "./commands";
+import {buildCommands, runCmd} from "./commands";
 import chalk from "chalk";
-import {ChildProcess, spawn, spawnSync} from "child_process";
+import {ChildProcess, spawn} from "child_process";
 import BuildConfig from "./config";
 
 program
@@ -14,6 +14,30 @@ program
         "Command line tool for building all or individual packages for production or in a development file watching mode (with windows terminal support)",
     );
 
+
+program
+    .command("run <command>")
+    .description("Run a specific command. Uses ids set in your build config. You can also pass multiple with a comma separating them which will run commands in that order. E.g. `build run someTypescriptCommand` or `build run command1,command2`")
+    .option("-c, --config <path>", "Use a different .buildrc file")
+    .option("-q, --quiet", "Pass this option to not print command output")
+    .option("-f, --from", "Relative path to use as the working directory")
+    .option("-p, --print", "Prints commands as they are run")
+    .action((cmd: string, options) => {
+        const cmds = cmd.split(",").map((c) => c.toLowerCase());
+        const configpath = resolve(process.cwd(), options.config || ".buildrc");
+        if (!existsSync(configpath)) {
+            console.log(chalk`{red !} Could not find file ${configpath}`);
+            return;
+        }
+        const config = JSON.parse(readFileSync(configpath, "utf-8"));
+        let commands = buildCommands(config, false);
+        commands = commands.filter((v) => cmds.includes(v.id.toLowerCase()));
+        for (const cmd of commands) {
+            if (cmd.id && (cmd.id && cmds.includes(cmd.id.toLowerCase()))) runCmd(cmd, options);
+        }
+    })
+
+
 program
     .command("build")
     .description("Run all build scripts in order")
@@ -21,6 +45,7 @@ program
     .option("-q, --quiet", "Pass this option to not print command output")
     .option("-f, --from", "Relative path to use as the working directory")
     .option("-p, --print", "Prints commands as they are run")
+    .option("-e, --exclude <command>", "Exclude a command or multiple commands separated by a comma. Uses id names")
     .action(async (options) => {
         const configpath = resolve(process.cwd(), options.config || ".buildrc");
         if (!existsSync(configpath)) {
@@ -29,28 +54,9 @@ program
         }
         const config = JSON.parse(readFileSync(configpath, "utf-8"));
         const commands = buildCommands(config, false);
+        const cmds = (options.exclude as string || "").split(",").map((c) => c.toLowerCase());
         for (const cmd of commands) {
-            console.log(chalk`{green.underline Building ${cmd.name}}`);
-            const cmdparts = cmd.command.split(" ");
-            console.log(chalk`{gray ${cmd.command}}`);
-            const s = spawnSync(<string>cmdparts.shift(), cmdparts, {
-                stdio: options.quiet ? undefined : "inherit",
-                cwd: options.from
-                    ? resolve(process.cwd(), options.from)
-                    : process.cwd(),
-            });
-            if (s.error) {
-                console.log(
-                    chalk`\n\n{red There was a problem while building ${cmd.name}.} {gray ${s.error.message}}`,
-                );
-                process.exit();
-            }
-            if (s.status !== null && s.status !== 0) {
-                console.log(
-                    chalk`\n\n{red There was a problem while building ${cmd.name}.} {gray code ${s.status}}`,
-                );
-                process.exit();
-            }
+            if (!cmd.id || (cmd.id && !cmds.includes(cmd.id.toLowerCase()))) runCmd(cmd, options);
         }
     });
 
@@ -63,6 +69,7 @@ program
     )
     .option("-f, --from", "Relative path to use as the working directory")
     .option("-c, --config <path>", "Use a different .buildrc file")
+    .option("-e, --exclude <command>", "Exclude a command or multiple commands separated by a comma. Uses id names")
     .action(async (options) => {
         const configpath = resolve(process.cwd(), options.config || ".buildrc");
         if (!existsSync(configpath)) {
@@ -73,6 +80,7 @@ program
             readFileSync(configpath, "utf-8"),
         );
         const commands = buildCommands(config, true);
+        const cmds = (options.exclude as string || "").split(",").map((c) => c.toLowerCase());
         if (
             process.platform === "win32" &&
             config.enableTerminal !== false &&
@@ -81,7 +89,7 @@ program
             let wtcmd = "";
 
             for (const cmd of commands) {
-                wtcmd += `new-tab --title ${cmd.name} --startingDirectory ./ ${cmd.command}; `;
+                if (!cmd.id || (cmd.id && !cmds.includes(cmd.id.toLowerCase()))) wtcmd += `new-tab --title ${cmd.name} --startingDirectory ./ ${cmd.command}; `;
             }
 
             wtcmd = wtcmd.replace(/; $/, "");
@@ -104,28 +112,30 @@ program
             let killing = false;
 
             for (const cmd of commands) {
-                const c = cmd.command.split(" ");
-                const pr = spawn(<string>c.shift(), c, {
-                    stdio: "inherit",
-                    cwd: options.from
-                        ? resolve(process.cwd(), options.from)
-                        : process.cwd(),
-                });
-                pr.on("close", (code) => {
-                    if (!killing) {
-                        if (code !== 0) {
-                            for (const p of proc) {
-                                p.c.kill();
-                                console.log(chalk`{red Killed ${cmd.name}}`);
+                if (!cmd.id || (cmd.id && !cmds.includes(cmd.id))) {
+                    const c = cmd.command.split(" ");
+                    const pr = spawn(<string>c.shift(), c, {
+                        stdio: "inherit",
+                        cwd: options.from
+                            ? resolve(process.cwd(), options.from)
+                            : process.cwd(),
+                    });
+                    pr.on("close", (code) => {
+                        if (!killing) {
+                            if (code !== 0) {
+                                for (const p of proc) {
+                                    p.c.kill();
+                                    console.log(chalk`{red Killed ${cmd.name}}`);
+                                }
+                            } else {
+                                console.log(
+                                    chalk`{green ${cmd.name} closed its self with code ${code}`,
+                                );
                             }
-                        } else {
-                            console.log(
-                                chalk`{green ${cmd.name} closed its self with code ${code}`,
-                            );
                         }
-                    }
-                });
-                proc.push({name: cmd.name, c: pr});
+                    });
+                    proc.push({name: cmd.name, c: pr});
+                }
             }
 
             const end = () => {
--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,15 @@
 import BuildConfig from "./config";
+import chalk from "chalk";
+import {spawnSync} from "child_process";
+import {resolve} from "path";
+
+export type ICommand = { name: string; command: string, id:string };
 
 export function buildCommands(
     config: BuildConfig,
     devmode: boolean,
-): { name: string; command: string }[] {
-    const commands: { name: string; command: string }[] = [];
+): ICommand[] {
+    const commands: ICommand[] = [];
     const prefix = config.packagePrefix ?? "";
     const yarncmd = process.platform === "win32" ? "yarn.cmd" : "yarn";
 
@@ -20,6 +25,7 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix,)} ${cmd.command}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 });
             } else if (
                 !devmode &&
@@ -28,6 +34,7 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix,)} ${cmd.devcommand}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 });
             }
         } else if (cmd.type === "typescript") {
@@ -41,6 +48,7 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix,)} tsc${doConf} -w`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 });
             } else if (
                 !devmode &&
@@ -49,6 +57,7 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix,)} tsc${doConf}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 });
             }
         } else if (cmd.type === "snowpack") {
@@ -56,11 +65,13 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} snowpack dev`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             } else if (!devmode && (cmd.runInEnvironments ?? ["dev", "prod"]).includes("prod")) {
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} snowpack build`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             }
         } else if (cmd.type === "storybook") {
@@ -68,11 +79,13 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} start-storybook -p ${cmd.port ?? 6006}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             } else if (!devmode && (cmd.runInEnvironments ?? ["dev", "prod"]).includes("prod")) {
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} build-storybook -o ${cmd.out ?? "docs"}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             }
         } else if(cmd.type === "next") {
@@ -80,15 +93,41 @@ export function buildCommands(
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} next dev -p ${cmd.port ?? 3000}`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             } else if (!devmode && (cmd.runInEnvironments ?? ["dev", "prod"]).includes("prod")) {
                 commands.push({
                     command: `${b(cmd.package, cmd.dontUsePackagePrefix)} next build`,
                     name: cmd.id ?? cmd.package,
+                    id: cmd.id,
                 })
             }
         }
     }
 
     return commands;
+}
+
+export function runCmd(cmd: ICommand, options: any) {
+    console.log(chalk`{green.underline Building ${cmd.name}}`);
+    const cmdparts = cmd.command.split(" ");
+    if (options.print) console.log(chalk`{gray ${cmd.command}}`);
+    const s = spawnSync(<string>cmdparts.shift(), cmdparts, {
+        stdio: options.quiet ? undefined : "inherit",
+        cwd: options.from
+            ? resolve(process.cwd(), options.from)
+            : process.cwd(),
+    });
+    if (s.error) {
+        console.log(
+            chalk`\n\n{red There was a problem while building ${cmd.name}.} {gray ${s.error.message}}`,
+        );
+        process.exit();
+    }
+    if (s.status !== null && s.status !== 0) {
+        console.log(
+            chalk`\n\n{red There was a problem while building ${cmd.name}.} {gray code ${s.status}}`,
+        );
+        process.exit();
+    }
 }
